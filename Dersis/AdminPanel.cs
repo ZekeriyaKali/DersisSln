7c1cd83902a151895ba903f5b79b1e7853eae79b
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2024-11-01 11:49:01 +0300

    Proje dosyası ekle.

---
+++ Dersis/AdminPanel.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Dersis
+{
+    public partial class AdminPanel : Form
+    {
+        public AdminPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdminPanelGeri_Click(object sender, EventArgs e)
+        {
+            Anasayfa anasayfa = new Anasayfa();
+            anasayfa.Show();
+            this.Hide();
+        }
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            if (tbxKullaniciAdi.Text == "admin" && tbxSifre.Text == "123")
+            {
+                MessageBox.Show("Giriş Başarılı!");
+                AdminIslemleri adminIslemleri = new AdminIslemleri();
+                adminIslemleri.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre Yanlış!");
+            }
+        }
+    }
+}
